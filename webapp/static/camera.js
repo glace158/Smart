ca152147ee7b21324573bd00ca2395ab306e6684
glace158 <jin7134@naver.com>
2022-05-11 21:22:51 +0900

    camera test

--- webapp/static/camera.js
+++ webapp/static/camera.js
@@ -27,21 +27,33 @@ function onloadcam(num){
 			document.getElementById("cam" + num).src="/video_feed/" + num + "/" + camstate + "?time" + time;
 		}
   }
-/*  
+
 $(function(){
 	onloadcam(0);
 	$('.select-area select').on('change', function(){
-		onloadcam($(this).val(), $(this).attr('data-select'));
+		onloadcam($(this).val();
+		$('.select-area'). val(src = "/video_feed/" + num + "/" + camstate + "?time" + time);
 	});
 });
 
-function onloadcam(num, layout){
-	var time = new Date().getTime();
-	var src;
-	src = "/video_feed/" + num + "/" + camstate + "?time" + time;
-	$('.' + layout).attr('src', src);
-}
-*/
+$(function(){
+	onloadcam(1);
+	$('.select-area select').on('change', function(){
+		onloadcam($(this).val();
+		$('.select-area'). val(src = "/motor/" + num + "/" + camstate + "?time" + time);
+	});
+});
+
+$(function(){
+	onloadcam(2);
+	$('.select-area select').on('change', function(){
+		onloadcam($(this).val();
+		$('.select-area'). val(src = "/servo/" + num + "/" + camstate + "?time" + time);
+	});
+});
+
+
+
 window.addEventListener("keydown", (e) => {
 	if(servo_keys.includes(e.key.toLowerCase())){
 		armcamstate = true;
